3aa087b593552e34a4c1e66108465f3bb0090b1e
mrunal <mrunal_khandekar@persistent.co.in>
2015-05-17 12:57:20 +0530

    knife.rb changed

--- knife.rb
+++ knife.rb
@@ -1,9 +1,9 @@
+current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                'autoport'
-client_key               '/root/chef-repo/.chef/autoport.pem'
+client_key               "#{current_dir}/Chef/autoport.pem"
 validation_client_name   'chef-validator'
-validation_key           '/root/chef-repo/.chef/chef-validator.pem'
+validation_key           "#{current_dir}/Chef/chef-validator.pem'
 chef_server_url          'https://ChefServer.persistent.co.in:443'
-syntax_check_cache_path  '/root/chef-repo/.chef/syntax_check_cache'
-cookbook_path [ '/root/chef-repo/cookbooks' ]
+cookbook_path [ "#{current_dir}/./" ]
