6c477eaaf41ead8968ac929b78de471700fcf478
mrunal <mrunal_khandekar@persistent.co.in>
2015-05-17 14:39:52 +0530

    knife.rb changed 4

--- knife.rb
+++ knife.rb
@@ -4,5 +4,5 @@ log_location             STDOUT
 node_name                'autoport'
 client_key               'Chef/autoport.pem'
 validation_client_name   'chef-validator'
-validation_key           'Chef/chef-validator.pem
+validation_key           'Chef/chef-validator.pem'
 chef_server_url          'https://ChefServer.persistent.co.in:443'
